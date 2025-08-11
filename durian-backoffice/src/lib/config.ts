0681e868490e5c7f0c2aa4b9b6cd807f079868ee
Dalvii <dalviorigine74@gmail.com>
2025-08-11 12:41:31 +0700

    use big_server on docker

--- durian-backoffice/src/lib/config.ts
+++ durian-backoffice/src/lib/config.ts
@@ -1,5 +1,5 @@
 // Configuration de l'API
-export const API_BASE_URL = "http://localhost:5001" // Changez cette URL selon votre environnement
+export const API_BASE_URL = "" // Changez cette URL selon votre environnement
 
 // Helper pour construire les URLs
 export const buildApiUrl = (endpoint: string) => `${API_BASE_URL}${endpoint}`
